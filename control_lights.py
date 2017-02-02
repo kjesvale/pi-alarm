f461a271f0cea2f57317c519f8b825224839e8b2
Kjetil Svalestuen <kjetsva@usit.uio.no>
2017-02-02 12:18:24 +0100

    hue: wakeup sequence

--- control_lights.py
+++ control_lights.py
@@ -7,29 +7,20 @@ hue_ip = user_config.readline().strip()
 user_id = user_config.readline().strip()
 
 base_url = 'http://' + hue_ip + '/api/' + user_id + '/'
-light_1 = base_url + 'lights/1/state'
-light_2 = base_url + 'lights/2/state'
 group = base_url + 'groups/1/action'
 
-delay = 1
+wakeup_duration = 30
+light_max_value = 254
+sleep_duration = (wakeup_duration * 60) / 254
 
-# Turn the lights on
-#requests.put(group_state, data = json.dumps({ 'on': True, 'bri': 254, 'sat': 254 }))
-print 'Turned the lights on.'
+# Initial light settings to ensure they're on and has the correct color temp.
+requests.put(group, data = json.dumps({ 'on': True, 'bri': bri, 'ct': 286 }))
 
-bri = 1
+print 'Wakeup sequence initialized.'
 
-#r = requests.get(base_url + 'lights/1/')
-r = requests.put(light_1, data = json.dumps({ 'on': True, 'bri': bri, 'ct': 286 }))
-
-print r.json()
-
-#while True:
-#    hue = 1
-#
-#    while (hue < 65000):
-#        r1 = requests.put(group_state, data = json.dumps({ 'hue': hue }))
-
-#        time.sleep(delay)
-#        print(r1.json())
-#        hue += 1000
+bri = 0
+while bri < 255:
+	result = requests.put(group, data = json.dumps({ 'bri': bri, }))
+	print bri + ':' + result.json()
+	time.sleep(sleep_duration)
+	bri += 1
