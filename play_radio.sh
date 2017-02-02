0425a5bd91bff916538a540ffe5e0f9c7340a65e
kjesvale <kjesvale@hotmail.com>
2017-02-02 21:42:53 +0100

    refactoring: updated speech engine and added radio

--- play_radio.sh
+++ play_radio.sh
@@ -2,6 +2,6 @@
 radio_station=`sed -n '1p' radio.config`
 duration=`sed -n '2p' radio.config`
 
-# Play station for 
-sudo timeout ${duration}s mplayer ${radio_station} >& trash
-rm trash
+# Play station for the given duration
+timeout ${duration}s mplayer ${radio_station} </dev/null >/dev/null 2>&1 &
+
--- speak.sh
+++ speak.sh
@@ -4,8 +4,9 @@ echo $1 > input
 read buffer < input
 
 # Compile and say input message.
-sudo pico2wave -w=speech.wav "${buffer}" >& trash
-sudo aplay speech.wav >& trash
+pico2wave -w=speech.wav "${buffer}"
+#aplay speech.wav &> /dev/null
+sox -v 0.9 speech.wav -r 48k speech.mp3 gain -6 treble +6 &> /dev/null
+mplayer speech.mp3 &> /dev/null
 
 rm input
-rm trash
